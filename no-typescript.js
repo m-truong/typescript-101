a989bce51d2e8f68e5b0ef52d7f2bd4c35755761
Michael Truong <59580072+m-truong@users.noreply.github.com>
2024-09-10 14:59:37 -0400

    add notes

--- no-typescript.js
+++ no-typescript.js
@@ -6,7 +6,10 @@ const fun = (a, b) => {
 
 // this gets executed
 // and save the data inside this variable
+// vanilla JS just dynamically determines the type without static-typing
 const dataOutput = add(2, 5);
 
 // IDE reserved keyword
 console.log(dataOutput)
+
+// ALWAYS need to use static-typing to inform larger codebase engineers that the TS function EXPECTS specific data-type
