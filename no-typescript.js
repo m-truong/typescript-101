07370b5942a531081a4edffdcb3d1736775acf0a
Michael Truong <59580072+m-truong@users.noreply.github.com>
2024-09-10 14:56:42 -0400

    add JS file

---
+++ no-typescript.js
@@ -0,0 +1,12 @@
+
+// defining a FatArrow => function
+const fun = (a, b) => {
+    return a + b;
+}
+
+// this gets executed
+// and save the data inside this variable
+const dataOutput = add(2, 5);
+
+// IDE reserved keyword
+console.log(dataOutput)
